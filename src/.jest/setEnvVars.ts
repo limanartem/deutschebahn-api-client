11fe1552a8b43baeb07773efcc1ce31a4b893ffb
limanartem81 <limanartem@gmail.com>
2024-05-16 11:49:48 +0200

    temp

--- src/.jest/setEnvVars.ts
+++ src/.jest/setEnvVars.ts
@@ -1,3 +1,4 @@
-process.env.DB_API_URL = 'http://127.0.0.1:5002';
+/* process.env.DB_API_URL = 'http://127.0.0.1:5002';
 process.env.DB_API_CLIENT_ID = 'test';
 process.env.DB_API_KEY = 'test';
+ */
