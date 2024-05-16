65ad6e39b7f7dda37b273d6d56132dfb80303187
limanartem81 <limanartem@gmail.com>
2024-05-16 11:54:21 +0200

    chore: Set environment variables for DB API in Jest test setup

--- src/.jest/setEnvVars.ts
+++ src/.jest/setEnvVars.ts
@@ -1,4 +1,5 @@
-/* process.env.DB_API_URL = 'http://127.0.0.1:5002';
-process.env.DB_API_CLIENT_ID = 'test';
-process.env.DB_API_KEY = 'test';
- */
+if (process.env.JEST_GROUP_INTEGRATION == null) {
+  process.env.DB_API_URL = 'http://127.0.0.1:5002';
+  process.env.DB_API_CLIENT_ID = 'test';
+  process.env.DB_API_KEY = 'test';
+}
